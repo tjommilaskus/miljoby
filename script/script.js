7fac5633cae7fe4e8a095ad810cd18043d9e3948
Tjommilaskus <mps.94@me.com>
2024-09-30 14:31:32 +0200

    endret masse

--- script/script.js
+++ script/script.js
@@ -1,7 +1,26 @@
-window.onload = function() {
-    fetch('navBar.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('navBar').innerHTML = data;
-      });
-  };
+document.addEventListener('DOMContentLoaded', () => {
+  const navBar = document.getElementById('navBar');
+  if (navBar) {
+    navBar.innerHTML = `
+      <nav>
+        <ul>
+          <li class="logo-item"><a href="index.html"><img src="bilder/hvl-topia-logo.svg" alt="HVLTopia Logo" class="logo"></a></li>
+          <li class="link-item"><a href="index.html">Hjem</a></li>
+          <li class="link-item"><a href="lesMer.html">Les Mer</a></li>
+          <li class="link-item"><a href="omOss.html">Om Oss</a></li>
+          <li class="link-item"><a href="kontaktOss.html">Kontakt oss</a></li>
+          <li class="link-item"><a href="berekraft.html">Bærekraft</a></li>
+        </ul>
+      </nav>
+    `;
+  }
+
+  // Legg til aktiv klasse på gjeldende side
+  const currentPage = window.location.pathname.split("/").pop();
+  const navLinks = document.querySelectorAll('nav ul li a');
+  navLinks.forEach(link => {
+    if (link.getAttribute('href') === currentPage) {
+      link.classList.add('active');
+    }
+  });
+});
